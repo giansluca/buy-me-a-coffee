b8eea28203c9c68a98a7aacd41937225d2046931
gians <giansluca@gmail.com>
2023-04-08 17:11:09 +0200

    update

--- test/utils/cryptography.test.js
+++ test/utils/cryptography.test.js
@@ -9,73 +9,75 @@ const {
     getAddressFromPublicKey,
 } = require("../../utils/cryptography");
 
-describe("Hash Message", function () {
-    it("should hash a message with sha256 algorithm", async function () {
-        // Given
-        const message = "hello world";
+describe("Cryptography test", function () {
+    describe("Hash Message", function () {
+        it("should hash a message with sha256 algorithm", async function () {
+            // Given
+            const message = "hello world";
 
-        // When
-        const hash = hashSha256(message);
-        console.log(`hex sha256 hash of: '${message}' is: ${toHex(hash)} `);
+            // When
+            const hash = hashSha256(message);
+            console.log(`hex sha256 hash of: '${message}' is: ${toHex(hash)} `);
 
-        // Then
-        expect(hash).to.not.null;
-        expect(hash).to.be.a("Uint8Array");
-        expect(hash).to.have.lengthOf(32);
+            // Then
+            expect(hash).to.not.null;
+            expect(hash).to.be.a("Uint8Array");
+            expect(hash).to.have.lengthOf(32);
+        });
+
+        it("should hash a message with keccak256 algorithm", async function () {
+            // Given
+            const message = "hello world";
+
+            // When
+            const hash = hashKeccak256(message);
+
+            console.log(`hex Keccak256 hash of: '${message}' is: ${toHex(hash)} `);
+
+            // Then
+            expect(hash).to.not.null;
+            expect(hash).to.be.a("Uint8Array");
+            expect(hash).to.have.lengthOf(32);
+        });
     });
 
-    it("should hash a message with keccak256 algorithm", async function () {
-        // Given
-        const message = "hello world";
+    describe("Sign Message", function () {
+        it("should sign a hash with secp256k1 algorithm and recover public key", async function () {
+            // Given
+            const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+            const publicKey = secp.getPublicKey(PRIVATE_KEY);
+            const message = "hello world";
 
-        // When
-        const hash = hashKeccak256(message);
+            // When
+            const hash = hashKeccak256(message);
+            const [signature, recoveryBit] = await signSecp256k1(hash, PRIVATE_KEY);
+            const recoveredPublicKey = recoverPublicKey(hash, signature, recoveryBit);
 
-        console.log(`hex Keccak256 hash of: '${message}' is: ${toHex(hash)} `);
+            const isSigned = secp.verify(signature, hash, recoveredPublicKey);
 
-        // Then
-        expect(hash).to.not.null;
-        expect(hash).to.be.a("Uint8Array");
-        expect(hash).to.have.lengthOf(32);
+            // Then
+            expect(isSigned).to.be.true;
+            expect(recoveryBit).to.not.null;
+            expect(recoveryBit).to.be.a("number");
+            expect(recoveredPublicKey).to.be.a("Uint8Array");
+            expect(recoveredPublicKey).has.length(65);
+            expect(toHex(publicKey)).to.be.equal(toHex(recoveredPublicKey));
+        });
     });
-});
 
-describe("Sign Message", function () {
-    it("should sign a hash with secp256k1 algorithm and recover public key", async function () {
-        // Given
-        const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
-        const publicKey = secp.getPublicKey(PRIVATE_KEY);
-        const message = "hello world";
+    describe("Sign Message", function () {
+        it("should extract the Ethereum address from public key", async function () {
+            // Given
+            const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+            const EXPECTED_ADDRESS = "16bB6031CBF3a12B899aB99D96B64b7bbD719705";
+            const publicKey = secp.getPublicKey(PRIVATE_KEY);
 
-        // When
-        const hash = hashKeccak256(message);
-        const [signature, recoveryBit] = await signSecp256k1(hash, PRIVATE_KEY);
-        const recoveredPublicKey = recoverPublicKey(hash, signature, recoveryBit);
+            // When
+            const address = getAddressFromPublicKey(publicKey);
 
-        const isSigned = secp.verify(signature, hash, recoveredPublicKey);
-
-        // Then
-        expect(isSigned).to.be.true;
-        expect(recoveryBit).to.not.null;
-        expect(recoveryBit).to.be.a("number");
-        expect(recoveredPublicKey).to.be.a("Uint8Array");
-        expect(recoveredPublicKey).has.length(65);
-        expect(toHex(publicKey)).to.be.equal(toHex(recoveredPublicKey));
-    });
-});
-
-describe("Sign Message", function () {
-    it("should extract the Ethereum address from public key", async function () {
-        // Given
-        const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
-        const EXPECTED_ADDRESS = "16bB6031CBF3a12B899aB99D96B64b7bbD719705";
-        const publicKey = secp.getPublicKey(PRIVATE_KEY);
-
-        // When
-        const address = getAddressFromPublicKey(publicKey);
-
-        // Then
-        expect(address).has.length(40);
-        expect(address.toUpperCase()).to.be.equal(EXPECTED_ADDRESS.toUpperCase());
+            // Then
+            expect(address).has.length(40);
+            expect(address.toUpperCase()).to.be.equal(EXPECTED_ADDRESS.toUpperCase());
+        });
     });
 });
