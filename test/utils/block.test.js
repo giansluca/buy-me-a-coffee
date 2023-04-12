64b4678457c0cdb3fce2a4721d3c2eda254cfbf0
gians <giansluca@gmail.com>
2023-04-12 23:24:00 +0200

    update

--- test/utils/block.test.js
+++ test/utils/block.test.js
@@ -1,4 +1,7 @@
 const { expect } = require("chai");
+const { faker } = require("@faker-js/faker");
+const SHA256 = require("crypto-js/sha256");
+const Hex = require("crypto-js/enc-hex");
 const Block = require("../../utils/block");
 
 describe("Block", function () {
@@ -11,5 +14,17 @@ describe("Block", function () {
 
         // Then
         expect(/^[0-9A-F]{64}$/i.test(hash)).to.be.true;
+    });
+
+    it("should hash some random data", function () {
+        // Given
+        const randomEmail = faker.internet.email();
+
+        // When
+        const myHash = SHA256(randomEmail);
+        const yourHash = new Block(randomEmail).toHash();
+
+        // Then
+        expect(Hex.stringify(myHash)).to.be.equal(Hex.stringify(yourHash));
     });
 });
---
+++ utils/blockchain.js
@@ -0,0 +1,11 @@
+const Block = require("./block");
+
+class Blockchain {
+    constructor() {
+        this.chain = [
+            /* TODO: Create the genesis block here */
+        ];
+    }
+}
+
+module.exports = Blockchain;
